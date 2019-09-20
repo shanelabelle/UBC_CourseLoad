9899883d667e689ef7dc15ee13a1ec43a4d6e506
smlabell <smlabell@ualberta.ca>
2019-09-19 22:02:10 -0700

    Created classes for User Setup, User, Course Load, Courses and built the user setup control loop

--- src/main/UI/Main.java
+++ src/main/UI/Main.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        // this is the main line
+
         UserSetup setup = new UserSetup();
 
         setup.getUserInfo();
